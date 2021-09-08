c736bb089bfd0114be537ee4839608949f1e0bce
wandersonDeve <Wanderlavo@outlook.com>
2021-09-07 22:16:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ app.use(express.json());
 
 const port = process.env.PORT || 8080;
 
+app.all("/*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", "*");
+
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
+
+    next();
+  });
+
 app.use("/", home);
 app.use("/personagens/all", readAll)
 app.use("/personagens/single", readById);
