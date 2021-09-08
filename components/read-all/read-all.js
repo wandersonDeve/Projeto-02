77c21b09dc46f42361f8480d2932e03f41f8704e
wandersonDeve <Wanderlavo@outlook.com>
2021-09-07 21:52:31 -0300

    Update read-all.js

--- components/read-all/read-all.js
+++ components/read-all/read-all.js
@@ -26,16 +26,24 @@ const router = express.Router();
   const personagens = db.collection("rickrick");
 
   const getPersonagensValidas = () => personagens.find({}).toArray();
-  const getPersonagemById = async (id) =>
-    personagens.findOne({ _id: ObjectId(id) });
+  const getPersonagemById = async (id) => personagens.findOne({ _id: ObjectId(id) });
 
-  router.use(function (req, res, next) {
+  router.all("/*", (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    res.header("Access-Control-Allow-Methods", "*");
+
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    );
+
     next();
   });
 
   router.get("/", async (req, res) => {
     res.send(await getPersonagensValidas());
-})
+  });
 })();
 
 module.exports = router;
